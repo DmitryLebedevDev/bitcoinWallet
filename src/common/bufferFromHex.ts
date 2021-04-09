b46d090853aad641ce626a072adbee57ef3c5995
Gercog52 <54477063+Gercog52@users.noreply.github.com>
2021-04-09 23:19:21 +0800

    refactoring

--- src/common/bufferFromHex.ts
+++ src/common/bufferFromHex.ts
@@ -2,6 +2,6 @@ export const bufferFromHex = (hexString: string) =>
   Buffer.from(
     new Uint8Array(
       (hexString.match(/.{1,2}/g) as string[] || [])
-      .map((byteHex) => parseInt(byteHex, 16))
+      .map(byteHex => parseInt(byteHex, 16))
     )
   )
