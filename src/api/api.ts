51095f3740ba3331ac5bb66a08d1f0361b6e7b73
Gercog52 <54477063+Gercog52@users.noreply.github.com>
2021-04-18 18:25:47 +0800

    refactoring

--- src/api/api.ts
+++ src/api/api.ts
@@ -6,14 +6,13 @@ import {
   IunspentTransactionsRes,
   IunspentTransaction
 } from './api.interface'
-import { address } from "bitcoinjs-lib";
 
 const api = axios.create({
   baseURL: 'https://testnet-api.smartbit.com.au/v1/blockchain/'
 })
 
 export const getAddressInfoReq
-  = (address: string) => api.get<IaddressInfoRes>(`address/${address}`);
+  = (address: string) => api.get<IaddressInfoRes>(`address/${address}`)
 export const getAddressBalanceReq = (address: string) => api.get<IaddressInfoRes>(
   `address/${address}`
 ).then(({
