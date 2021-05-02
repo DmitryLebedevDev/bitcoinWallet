ace9f07c8ea8379c91a0a1cd7709298ecd34ea43
Gercog52 <54477063+Gercog52@users.noreply.github.com>
2021-05-02 22:18:03 +0800

    refactoring style

--- src/pages/home/TransactionsTable/Transaction/Transaction.tsx
+++ src/pages/home/TransactionsTable/Transaction/Transaction.tsx
@@ -1,9 +1,18 @@
-import React, { FC, useCallback, useState } from "react";
+import React, { FC, useCallback, useState } from "react"
 import { Itransaction } from '../../../../api/api.interface'
-import { IconButton, TableCell, TableRow } from "@material-ui/core"
+import { IconButton, makeStyles, TableCell, TableRow } from "@material-ui/core";
 import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp"
 import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown"
 import { SuccessTag } from "../Statuses/SuccessTag"
+import Collapse from '@material-ui/core/Collapse'
+
+const useStyles = makeStyles({
+  utxoTableWrap: {
+    paddingBottom: 0,
+    paddingTop: 0,
+    border: "none"
+  }
+})
 
 export const Transaction:FC<Itransaction> = (
   {txid, hash, fee, confirmations, outputs}
@@ -14,30 +23,40 @@ export const Transaction:FC<Itransaction> = (
     [setIsOpen]
   )
 
+  const classes = useStyles();
   return (
-    <TableRow key={txid}>
-      <TableCell>
-        <IconButton onClick={handleClickOpenBtn}
-                    size="small"
-        >
-          {isOpen ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon/>}
-        </IconButton>
+    <>
+      <TableRow key={txid}>
+        <TableCell>
+          <IconButton onClick={handleClickOpenBtn}
+                      size="small"
+          >
+            {isOpen ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon/>}
+          </IconButton>
+        </TableCell>
+        <TableCell>
+          {txid}
+        </TableCell>
+        <TableCell>
+          {hash}
+        </TableCell>
+        <TableCell>
+          {fee}
+        </TableCell>
+        <TableCell>
+          {confirmations}
+        </TableCell>
+        <TableCell>
+          <SuccessTag/>
+        </TableCell>
+      </TableRow>
+      <TableCell className={classes.utxoTableWrap}>
+        <Collapse in={isOpen}>
+          <div>
+            test
+          </div>
+        </Collapse>
       </TableCell>
-      <TableCell>
-        {txid}
-      </TableCell>
-      <TableCell>
-        {hash}
-      </TableCell>
-      <TableCell>
-        {fee}
-      </TableCell>
-      <TableCell>
-        {confirmations}
-      </TableCell>
-      <TableCell>
-        <SuccessTag/>
-      </TableCell>
-    </TableRow>
+    </>
   )
 }
